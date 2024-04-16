ba6681b7eaa6db8f9930104ac268aabcb786ee60
juhalexandrino <juliasalexandrino@hotmail.com>
2024-04-16 01:04:46 -0300

    Adicionando models Postagem, Temas e Comentarios

---
+++ Model/Auditable.cs
@@ -0,0 +1,7 @@
+﻿namespace marvelHub.Model
+{
+    public class Auditable
+    {
+        public DateTimeOffset Data { get; set; }
+    }
+}
---
+++ Model/Postagem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace marvelHub.Model
+{
+    public class Postagem : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(2000)]
+        public string Texto { get; set; } = string.Empty;
+
+    }
+}
