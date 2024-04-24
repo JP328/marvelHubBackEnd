9973b414ed51e598c0fbcc8c3225ba8f73f3ec77
juhalexandrino <juliasalexandrino@hotmail.com>
2024-04-24 18:26:03 -0300

    Adicionando as intefarces e classes de serviço para as tabelas Postagem e Comentário

---
+++ Service/IPostService.cs
@@ -0,0 +1,19 @@
+﻿using marvelHub.Model;
+
+namespace marvelHub.Service
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<Post>> GetAll();
+
+        Task<Post?> GetById(long id);
+
+        Task<IEnumerable<Post>> GetByTitle(string title);
+
+        Task<Post?> Create(Post post);
+
+        Task<Post?> Update(Post post);
+
+        Task Delete(Post post);
+    }
+}
---
+++ Service/Implements/CommentService.cs
@@ -0,0 +1,98 @@
+﻿using marvelHub.Data;
+using marvelHub.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace marvelHub.Service.Implements
+{
+    public class CommentService : ICommentService
+    {
+        private readonly AppDbContext _context;
+
+        public CommentService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Comment>> GetByPost(Post post)
+        {
+            try
+            {
+                return await _context.Comments
+                   .Include(c => c.Post)
+                   .Where(c => c.Post.Id == post.Id)
+                   .ToListAsync();
+            }
+            catch { return null; }
+        }
+        public async Task<IEnumerable<Comment>> GetByUserId(string userid)
+        {
+            try
+            {
+                 return await _context.Comments
+                    .Include(c => c.Post)
+                    .Where(c => c.UserId == userid)
+                    .ToListAsync();
+            }
+            catch { return null; }
+        }
+        public async Task<Comment?> GetById(long id)
+        {
+            try
+            {
+                var Comment = await _context.Comments
+                    .Include(c => c.Post)
+                    .FirstAsync(c => c.Id == id);
+                
+                return Comment;
+            }
+            catch { return null; }
+        }
+        public async Task<Comment?> Create(Comment comment)
+        {
+            if (comment.Post is not null)
+            {
+                var CheckPost = await _context.Posts.FindAsync(comment.Post.Id);
+
+                if (CheckPost is null)
+                    return null;
+
+                comment.Post = CheckPost;
+            }
+
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+
+            return comment;
+        }
+        public async Task<Comment?> Update(Comment comment)
+        {
+            var CommentUpdate = await _context.Posts.FindAsync(comment.Id);
+
+            if (comment.Post is null)
+                return null;
+
+            if (comment.Post is not null)
+            {
+                var CheckPost = await _context.Posts.FindAsync(comment.Post.Id);
+
+                if (CheckPost is null)
+                    return null;
+
+                comment.Post = CheckPost;
+            }
+
+            if (CommentUpdate is not null)
+                _context.Entry(CommentUpdate).State = EntityState.Detached;
+            
+            _context.Entry(comment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return comment;
+        }
+        public async Task Delete(Comment comment)
+        {
+            _context.Remove(comment);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
